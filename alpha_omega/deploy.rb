974d740239bf51e65a1935c03e3dd038fb0f9b6d
Tom Bombadil <amanibhavam@destructuring.org>
2011-08-26 05:25:38 +0000

    remove instructions to set scm and deploy_via

--- alpha_omega/deploy.rb
+++ alpha_omega/deploy.rb
@@ -193,11 +193,6 @@ namespace :deploy do
     servers. You will rarely call this task directly, however; instead, you \
     should call the `deploy' task (to do a complete deploy) or the `update' \
     task (if you want to perform the `restart' task separately).
-
-    You will need to make sure you set the :scm variable to the source \
-    control software you are using (it defaults to :subversion), and the \
-    :deploy_via variable to the strategy you want to use to deploy (it \
-    defaults to :checkout).
   DESC
   task :update_code, :except => { :no_release => true } do
     dirs = [ releases_path, "#{deploy_to}/log" ]
