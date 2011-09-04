43135b0c8b9039a744761832e28a409bd373d71c
Tom Bombadil <amanibhavam@destructuring.org>
2011-09-04 06:25:31 +0000

    added major script, generalized deploy script, setup HeSYINUvSBZfxqA-foreman args passing

--- Procfile
+++
@@ -1,5 +0,0 @@
-git: git daemon --reuseaddr --base-path=. --export-all --verbose
-cap: bundle exec cap deploy
-rollback: bundle exec cap deploy:rollback
-stage: bundle exec cap deploy:update_code
-patch: bundle exec cap deploy
---
+++ Procfile.rb
@@ -0,0 +1,8 @@
+<<HERE
+git: git daemon --reuseaddr --base-path=. --export-all --verbose
+deploy:   bundle exec cap deploy
+major:    bundle exec cap deploy
+patch:    bundle exec cap deploy
+rollback: bundle exec cap deploy:rollback
+stage:    bundle exec cap deploy:update_code
+HERE
