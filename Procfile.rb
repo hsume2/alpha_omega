32b6937f7066dfcca5aa70b2151b2f4f3f312a8f
Tom Bombadil <amanibhavam@destructuring.org>
2011-09-26 20:04:39 +0000

    compare command

--- Procfile.rb
+++ Procfile.rb
@@ -5,4 +5,5 @@ major:    bundle exec cap deploy
 patch:    bundle exec cap deploy
 rollback: bundle exec cap deploy:rollback
 stage:    bundle exec cap deploy:update_code
+compare:  bundle exec cap deploy:compare
 HERE
