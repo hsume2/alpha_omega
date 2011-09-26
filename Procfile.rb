2c226ec80ea196bc730d11f10a731f3fd0336583
Tom Bombadil <amanibhavam@destructuring.org>
2011-09-26 20:25:34 +0000

    arguments inserted before deploy task

--- Procfile.rb
+++ Procfile.rb
@@ -1,11 +1,11 @@
 <<HERE
 git: git daemon --reuseaddr --base-path=. --export-all --verbose
-deploy:   bundle exec cap deploy
-major:    bundle exec cap deploy
-patch:    bundle exec cap deploy
-rollback: bundle exec cap deploy:rollback
-stage:    bundle exec cap deploy:update_code
-compare:  bundle exec cap deploy:compare
-shell:    bundle exec cap shell
+deploy:   bundle exec cap #{options[:args].join(" ")} deploy
+major:    bundle exec cap #{options[:args].join(" ")} deploy
+patch:    bundle exec cap #{options[:args].join(" ")} deploy
+rollback: bundle exec cap #{options[:args].join(" ")} deploy:rollback
+stage:    bundle exec cap #{options[:args].join(" ")} deploy:update_code
+compare:  bundle exec cap #{options[:args].join(" ")} deploy:compare
+shell:    bundle exec cap #{options[:args].join(" ")} shell
 HERE
 
