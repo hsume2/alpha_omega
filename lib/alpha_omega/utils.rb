95bd75f952fa365c2027109ac932c60c3c7de8ff
Tom Bombadil <amanibhavam@destructuring.org>
2012-02-08 23:35:26 +0000

    adding a new yaml target

--- lib/alpha_omega/utils.rb
+++ lib/alpha_omega/utils.rb
@@ -3,26 +3,67 @@ require 'yaml'
 
 module AlphaOmega
 
-  def self.echo_magic
+  def self.magic_prefix
     "eea914aaa8dde6fdae29242b1084a2b0415eefaf"
   end
 
+  def self.node_defaults(node, pods_config, opsdb, env_pod, this_pod, node_name)
+    node["node_name"] = node_name
+
+    # defaults
+    node["run_list"] ||= []
+    node["cap_group"] ||= []
+    node["nagios_group"] ||= []
+    node["private_aliases"] ||= []
+    node["node"] = nil
+
+    # enrich with pods config
+    node["env_pod"] = env_pod
+    node.deep_merge!(pods_config[env_pod])
+
+    # enrich with opsdb
+    node.deep_merge!(opsdb[env_pod][node_name])
+
+    node["run_list"] = node["run_list"].clone # TODO without a clone, node.run_list also updates pods_config.env_pod.run_list
+
+    # derive
+    node["fq_domain"] = %w(env_pod env_dc dc_domain).collect {|s| node[s] }.uniq.join(".")
+    node["fq_name"] = %w(node_name env_pod env_dc dc_domain).collect {|s| node[s] }.uniq.join(".")
+    node["p_name"] = "#{node["node_name"]}.#{node["env_pod"]}"
+
+    # check if managed
+    if "default" != env_pod # TODO get rid of default, use this_pod
+      node["q_name"] = "#{node["node_name"]}.#{node["env_pod"]}"
+      node["managed"] = true
+    else
+      node["q_name"] = node["node_name"]
+      node["managed"] = false
+    end
+
+    # check if infra pod
+    if node["env_pod"] == node["env_dc"]
+      node["infra"] = true
+    else
+      node["infra"] = false
+      node["infra_domain"] = "#{node["env_dc"]}.#{node["dc_domain"]}"
+    end
+
+    node["run_list"].concat pods_config[env_pod]["run_list"] if pods_config[env_pod].key? "run_list"
+
+    node
+  end
+
   def self.default_pods_tasks
-    Proc.new do |config, pod_name, pod, mix_pods|
-      # world task accumulates all.* after tasks
-      config.task "world" do
-      end
+    Proc.new do |config, pod_name, pod, mix_pods, pods_config, opsdb, this_pod|
+      [ "", ".echo", ".yaml" ].each do |tsuffix|
+         # world task accumulates all.* after tasks
+        config.task "world#{tsuffix}" do
+        end
 
-      config.task "world.echo" do
-      end
-
-      # each pod task sets the pod context for host/group tasks
-      config.task pod_name do
-        set :current_pod, pod_name
-      end
-
-      config.task "#{pod_name}.echo" do
-        set :current_pod, pod_name
+        # each pod task sets the pod context for host/group tasks
+        config.task "#{pod_name}#{tsuffix}" do
+          set :current_pod, pod_name
+        end
       end
 
       hosts =
@@ -31,30 +72,31 @@ module AlphaOmega
             role :app, remote_name
           end
         
-          config.task task_name do
-            after task_name, "#{task_name}.#{current_pod}"
-          end
-
           config.task "#{task_name}.#{pod_name}.echo" do
-            puts "#{AlphaOmega.echo_magic} #{remote_name}"
+            puts "#{AlphaOmega.magic_prefix} #{remote_name}"
           end
         
-          config.task "#{task_name}.echo" do
-            after "#{task_name}.echo", "#{task_name}.#{current_pod}.echo"
+          config.task "#{task_name}.#{pod_name}.yaml" do
+            n = self.node_defaults(node, pods_config. opsdb, pod_name, this_pod, task_name)
+            StringIO.new({ remote_name => n }.to_yaml).lines.each {|l| puts "#{AlphaOmega.magic_prefix} #{l}" }
           end
-
+        
+          [ "", ".echo", ".yaml" ].eeach do |tsuffix|
+            config.task "#{task_name}#{tsuffix}" do
+              after "#{task_name}#{tsuffix}", "#{task_name}.#{current_pod}#{tsuffix}"
+            end
+          end
         end
 
       AlphaOmega.what_groups hosts do |task_name, nodes|
         if task_name == "all"
           # simulate all podXX all
-          unless pod_name == "default"
-            config.after "world", pod_name
-            config.after "world.echo", "#{pod_name}.echo"
+          [ "", ".echo", ".yaml" ].each do |tsuffix|
+            unless pod_name == "default"
+              config.after "world#{tsuffix}", "#{pod_name}#{tsuffix}"
+            end
+            config.after "world#{tsuffix}", "#{task_name}#{tsuffix}"
           end
-          
-          config.after "world", task_name
-          config.after "world.echo", "#{task_name}.echo"
         end
 
         config.task "#{task_name}.#{pod_name}" do
@@ -71,10 +113,6 @@ module AlphaOmega
           end
         end
 
-        config.task task_name do
-          after task_name, "#{task_name}.#{current_pod}"
-        end
-
         config.task "#{task_name}.#{pod_name}.echo" do
           unless mix_pods
             if last_pod && last_pod != pod_name
@@ -85,19 +123,38 @@ module AlphaOmega
 
           set :last_pod, pod_name
           nodes.keys.sort.each do |remote_name|
-            puts "#{AlphaOmega.echo_magic} #{remote_name}"
+            puts "#{AlphaOmega.magic_prefix} #{remote_name}"
           end
         end
 
-        config.task "#{task_name}.echo" do
-          after "#{task_name}.echo", "#{task_name}.#{current_pod}.echo"
+        config.task "#{task_name}.#{pod_name}.yaml" do
+          unless mix_pods
+            if last_pod && last_pod != pod_name
+              puts "ERROR: cannot call tasks that mix different dc_env (last pod = #{last_pod}, current pod = #{pod_name})"
+              exit 1
+            end
+          end
+
+          set :last_pod, pod_name
+          nodes.keys.sort.each do |remote_name|
+            n = self.node_defaults(node, pods_config. opsdb, pod_name, this_pod, task_name)
+            StringIO.new({ remote_name => n }.to_yaml).lines.each {|l| puts "#{AlphaOmega.magic_prefix} #{l}" }
+          end
+        end
+
+        [ "", ".echo", ".yaml" ].each do |tsuffix|
+          config.task "#{task_name}#{tsuffix}" do
+            after "#{task_name}#{tsuffix}", "#{task_name}.#{current_pod}#{tsuffix}"
+          end
         end
       end
     end
   end
 
   def self.setup_pods (config, node_home, mix_pods = true)
-    self.what_pods(config, node_home) { |config, pod_name, pod| self.default_pods_tasks.call(config, pod_name, pod, mix_pods) }
+    self.what_pods(config, node_home) do |config, pod_name, pod, pods_config, opsdb, this_pod| 
+      self.default_pods_tasks.call(config, pod_name, pod, mix_pods, pods_config, opsdb, this_pod) 
+    end
   end
 
   def self.what_branch (allowed = %w(production master develop))
@@ -126,6 +183,16 @@ module AlphaOmega
   end
 
   def self.what_pods (config, node_home)
+    # pods config
+    pods_config = YAML.load(File.read("#{node_home}/config/pods.yml"))
+
+    # opsdb config
+    opsdb = Dir["#{node_home}/config/pod/*.yaml"].inject({}) do |acc, fname|
+      env_pod = File.basename fname, ".yaml"
+      acc[env_pod] = YAML.load(File.read(fname))
+      acc
+    end
+
     pods = { }
 
     this_pod = File.read("/etc/podname").strip
@@ -133,7 +200,7 @@ module AlphaOmega
       "nodes_spec" => "#{node_home}/pods/#{this_pod}/*.yaml",
       "node_suffix" => ""
     }
-    yield config, "default", pods[this_pod] # TODO get rid of default and use this_pod
+    yield config, "default", pods[this_pod], pods_config. opsdb, this_pod # TODO get rid of default and use this_pod
 
     this_host = Socket.gethostname.chomp.split(".")[0]
     this_node = YAML.load(File.read("#{node_home}/pods/#{this_pod}/#{this_host}.yaml"))
@@ -143,7 +210,7 @@ module AlphaOmega
         "nodes_spec" => "#{node_home}/pods/#{pod_name}/*.yaml",
         "node_suffix" => ".#{pod_name}"
       }
-      yield config, pod_name, pods[pod_name]
+      yield config, pod_name, pods[pod_name], pods_config, opsdb, this_pod
     end
 
     pods
@@ -187,6 +254,4 @@ module AlphaOmega
 
     cap_groups
   end
-
 end
-
