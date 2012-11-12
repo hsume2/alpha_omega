ec1f84fa015b1773cd48a806f3890cbe91e5fe2c
Tom Bombadil <amanibhavam@destructuring.org>
2012-11-12 13:29:15 -0800

    check for version before challenge

--- lib/alpha_omega/config/deploy_challenge.rb
+++ lib/alpha_omega/config/deploy_challenge.rb
@@ -4,17 +4,16 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
   namespace :deploy do
     task :challenge do
       if dna["app_env"] == "production"
+        unless ENV['FLAGS_tag'] && !(ENV['FLAGS_tag'].empty? || ENV['FLAGS_tag'] == "HEAD")
+          puts "Did not specify a tag for production via -t vX.Y.Z"
+          abort
+        end
 
         a, b = rand(10), rand(10)
         if Capistrano::CLI.ui.ask(" -- WARNING: Accessing production, please think: #{a} + #{b} = ").downcase.strip.to_i != (a + b)
           abort
         else
           sleep(3)
-        end
-
-        unless ENV['FLAGS_tag'] && !(ENV['FLAGS_tag'].empty? || ENV['FLAGS_tag'] == "HEAD")
-          puts "Did not specify a tag for production"
-          abort
         end
       end
     end
