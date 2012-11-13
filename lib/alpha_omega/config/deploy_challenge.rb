0a7051f87b575f77149488579995cc613e223f5a
Tom Bombadil <amanibhavam@destructuring.org>
2012-11-12 19:18:35 -0800

    allow --reviewer for non-current deploys

--- lib/alpha_omega/config/deploy_challenge.rb
+++ lib/alpha_omega/config/deploy_challenge.rb
@@ -9,11 +9,13 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
           abort
         end
 
-        a, b = rand(10), rand(10)
-        if Capistrano::CLI.ui.ask(" -- WARNING: Accessing production, please think: #{a} + #{b} = ").downcase.strip.to_i != (a + b)
-          abort
-        else
-          sleep(3)
+        unless ENV['FLAGS_reviewer'] && active_path_name == current_path_name
+          a, b = rand(10), rand(10)
+          if Capistrano::CLI.ui.ask(" -- WARNING: Accessing production, please think: #{a} + #{b} = ").downcase.strip.to_i != (a + b)
+            abort
+          else
+            sleep(3)
+          end
         end
       end
     end
