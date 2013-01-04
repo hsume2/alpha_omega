9f2542933ec9d24f3ba954b19236bb2f7ebc7810
Henry Hsu <hhsu@zendesk.com>
2013-01-03 17:35:54 -0800

    Adds option to disable flags tag in deploy.yml for gamma deploy

--- lib/alpha_omega/config/deploy_challenge.rb
+++ lib/alpha_omega/config/deploy_challenge.rb
@@ -3,12 +3,14 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
 
   namespace :deploy do
     task :challenge do
-      if dna["app_env"] == "production"
+      if dna["app_env"] == "production" && (!dna["cap_group"].include?("gamma") || $deploy["flags_tag"] != false)
         unless ENV['FLAGS_tag'] && !(ENV['FLAGS_tag'].empty? || ENV['FLAGS_tag'] == "HEAD")
           puts "Did not specify a tag for production via -t vX.Y.Z"
           abort
         end
+      end
 
+      if dna["app_env"] == "production"
         unless ENV['FLAGS_reviewer'] && active_path_name == current_path_name
           a, b = rand(10), rand(10)
           if Capistrano::CLI.ui.ask(" -- WARNING: Accessing production, please think: #{a} + #{b} = ").downcase.strip.to_i != (a + b)
